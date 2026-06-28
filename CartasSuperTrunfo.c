6f18a4b304603079db32a8d9b1b41519cd96c21f
Pablo Louback <pabloloubackoficial@gmail.com>
2026-06-28 18:23:07 -0400

    Nivel Intermediário - PIB per capita e Densidade demografica

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,13 @@ int main(){
     char nome_cidade_carta1[50], nome_cidade_carta2[50]; //o nome da cidade 
     int populacao_carta1=0, populacao_carta2=0, pturisticos_carta1 = 0, pturisticos_carta2 = 0;  // população de cada cidade e qtd de pontos turisticos.
     float area_carta1 = 0, area_carta2 = 0, pib_carta1 = 0, pib_carta2 = 0; //tamanho em km2 e pib.
+    //Nível Intermediário:
+    /*Calcular a Densidade Populacional: Divida a população da cidade pela sua área. O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado. Armazene esse valor em uma variável do tipo float.
+    Calcular o PIB per Capita: Divida o PIB da cidade pela sua população. O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade. Armazene esse valor em uma variável do tipo float.
+    Exibir os Resultados: Além de exibir as informações lidas do usuário (como no nível básico), seu programa também deve exibir a densidade populacional e o PIB per capita calculados para cada cidade. Formate os valores de ponto flutuante com duas casas decimais.*/
+    float densidade_populacional_carta1 = 0, densidade_populacional_carta2 = 0, pib_per_capita_carta1 = 0, pib_per_capita_carta2 = 0;
+
+
 
     printf("Prossiga com as informações da Carta 1!!!!!\n");
 
@@ -54,7 +61,23 @@ int main(){
     printf("\nDigite o pib da cidade %s:", nome_cidade_carta2);
     scanf("%f", &pib_carta2);
 
-    printf("CARTA 1:\n ESTADO: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nNumero de Pontos Túristicos: %d\n", estado_carta1,codigo_carta1,nome_cidade_carta1,populacao_carta1,area_carta1,pib_carta1,pturisticos_carta1);
-    printf("\nCARTA 2:\n ESTADO: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nNumero de Pontos Túristicos: %d", estado_carta2,codigo_carta2,nome_cidade_carta2,populacao_carta2,area_carta2,pib_carta2,pturisticos_carta2);
+    //calculo pibpercapita e densidade populacional
+    densidade_populacional_carta1 = populacao_carta1 / area_carta1;
+    densidade_populacional_carta2 = populacao_carta2 / area_carta2;
+    pib_per_capita_carta1 = pib_carta1 / populacao_carta1;
+    pib_per_capita_carta2 = pib_carta2 / populacao_carta2;
+
+    printf("\n\n");
+
+    printf("Obrigado pelos dados inseridos!\n");
+    printf("Estamos processando suas informações...\n");
+    printf("...\n");
+    printf("...\n");
+    printf("Quase lá...\n");
+    printf("...\n");
+    
+    printf("DADOS CARTA 1:\nESTADO: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nNumero de Pontos Túristicos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f", estado_carta1,codigo_carta1,nome_cidade_carta1,populacao_carta1,area_carta1,pib_carta1,pturisticos_carta1,densidade_populacional_carta1,pib_per_capita_carta1);
+    printf("\n\n");
+    printf("\nDADOS CARTA 2:\nESTADO: %s\nCódigo: %s\nNome da cidade: %s\nPopulação: %d\nÁrea: %.2f\nPIB: %.2f\nNumero de Pontos Túristicos: %d\nDensidade Populacional: %.2f\nPIB per Capita: %.2f", estado_carta2,codigo_carta2,nome_cidade_carta2,populacao_carta2,area_carta2,pib_carta2,pturisticos_carta2,densidade_populacional_carta2,pib_per_capita_carta2);
 
 }
